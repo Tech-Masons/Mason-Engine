e6f9c2b863a28d9dab234ce200374265a8ad1b7f
Lii02 <lukeinlow@gmail.com>
2022-07-16 18:19:05 -0400

    Fixed entity function names

--- Codebase/modules/core/core/src/ecs/entity.h
+++ Codebase/modules/core/core/src/ecs/entity.h
@@ -7,9 +7,9 @@
 struct Entity {
 	matrix4f world;
 public:
-	void set_position(float3 pos) { world.row3 = { pos.x, pos.y, pos.z, world.row3.w }; }
-	float3 position() { return world.row3.xyz(); }
-	float3 right() { return world.row0.xyz(); }
-	float3 up() { return world.row1.xyz(); }
-	float3 forward() { return world.row2.xyz(); }
+	void SetPosition(float3 pos) { world.row3 = { pos.x, pos.y, pos.z, world.row3.w }; }
+	float3 Position() { return world.row3.xyz(); }
+	float3 Right() { return world.row0.xyz(); }
+	float3 Up() { return world.row1.xyz(); }
+	float3 Forward() { return world.row2.xyz(); }
 };
